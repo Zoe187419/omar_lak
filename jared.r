0977d2659334562dff9bc985eba7f35aae7ef8a7
jared <jaredtritz@gmail.com>
2014-10-09 16:39:44 -0400

    done making the pairwise matching work for ecoach data

--- jared.r
+++ jared.r
@@ -9,7 +9,8 @@ options(digits = 3)
 set.seed(80)
 sessionInfo()
 
-# config
+# config learning communities
+version = 'lc'
 ifile = "../data_lak_2015/learning_communities2.csv"
 treat_col = 'treat'
 treat_val = 1
@@ -20,7 +21,23 @@ dep_var_col = 'CUM_GPA'
 prog_formula = paste0(dep_var_col, ' ~.')
 prop_formula = paste0('factor(', treat_col, ')~.')
 
+# config mcdb ecoach
+version = 'ec'
+ifile = "../data_lak_2015/mcdb.csv"
+treat_col = 'usage.ratio_xxx'
+treat_val = 'True'
+control_val = 'False'
+dep_var_col = 'Total.Points'
+# QUESTION: do binary trees work to predict continuous variables?
+# if so is it just by making it into lots of categorical bins?
+# QUESTION: is it ok to use data available only for treatment
+# when predicting the propensity
+prog_formula = paste0(dep_var_col, ' ~.')
+prop_formula = paste0('factor(', treat_col, ')~.')
+
 # config model
+hard_reset = FALSE
+hard_reset = TRUE
 tree.number = 2500
 
 # read and clean data
@@ -33,23 +50,23 @@ temp = na.omit(idat)
 #temp = rbind(low.data,high.data) # concat the lows back on... effectively sorted to the bottom.
 
 # build the prognostic score only on control data
-if(!file.exists('forest1.Rdata')) {
+if(hard_reset || !file.exists(paste0(version, '_forest1.Rdata'))) {
     cont.mod.rf = randomForest(
         as.formula(prog_formula), # http://stats.stackexchange.com/questions/10712/what-is-the-meaning-of-the-dot-in-r
         data = temp[which(temp[[treat_col]] == control_val),],
         ntree = tree.number,
         importance = TRUE
     )
-    save(cont.mod.rf, file='forest1.Rdata')
+    save(cont.mod.rf, file=paste0(version, '_forest1.Rdata'))
 } else {
-    load('forest1.Rdata')
+    load(paste0(version, '_forest1.Rdata'))
 }
 
 #varImpPlot(cont.mod.rf)
 vars = setdiff(colnames(temp), c(dep_var_col))
 
 # propensity score model
-if(!file.exists('forest2.Rdata')) {
+if(hard_reset || !file.exists(paste0(version, '_forest2.Rdata'))) {
     ppty.mod.rf = randomForest(
         as.formula(prop_formula),
         data = temp[,vars],
@@ -57,9 +74,9 @@ if(!file.exists('forest2.Rdata')) {
         importance = TRUE,
         norm.votes = TRUE
     )
-    save(ppty.mod.rf, file='forest2.Rdata')
+    save(ppty.mod.rf, file=paste0(version, '_forest2.Rdata'))
 } else {
-    load('forest2.Rdata')
+    load(paste0(version, '_forest2.Rdata'))
 }
 
 #varImpPlot(ppty.mod.rf)
@@ -84,6 +101,10 @@ fm.1.2 = pairmatch(factor(temp[[treat_col]]) ~ ppty.scores , data = temp)
 #print(summary(fm.1.2))
 #cat('--------------------\n\n')
 
+#print(temp[which(fm.1.1!='NA'),])
+print(table(fm.1.1))
+stop('exit')
+
 # pair match based on co-variates only
 fm.1.3 = pairmatch(factor(temp[[treat_col]]) ~ Sex+Init.Ethnic.Group.Descrshort+Init.Citizenship.Status.Descrs+
                                         stt+Hours.at.entry+Appl.Ctr.Rtng.Cmp1.Val.Descrsh+
@@ -91,5 +112,3 @@ fm.1.3 = pairmatch(factor(temp[[treat_col]]) ~ Sex+Init.Ethnic.Group.Descrshort+
 #print(summary(fm.1.3))
 #cat('--------------------\n\n')
 
-# temp[which(fm.1.1!='NA'),]
-stop('exit')
