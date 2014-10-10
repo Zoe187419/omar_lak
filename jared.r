9bfd21c666502f918118abcf7980631dea040b12
jared <jaredtritz@gmail.com>
2014-10-09 22:14:22 -0400

    done running models on all 3 data sets, modest config for each one

--- jared.r
+++ jared.r
@@ -9,17 +9,7 @@ options(digits = 3)
 set.seed(80)
 sessionInfo()
 
-# config learning communities
-version = 'lc'
-ifile = "../data_lak_2015/learning_communities2.csv"
-treat_col = 'treat'
-treat_val = 1
-control_val = 0
-dep_var_col = 'CUM_GPA'
-# QUESTION: do binary trees work to predict continuous variables?
-# if so is it just by making it into lots of categorical bins?
-prog_formula = paste0(dep_var_col, ' ~.')
-prop_formula = paste0('factor(', treat_col, ')~.')
+hard_reset = FALSE
 
 # config mcdb ecoach
 version = 'ec'
@@ -32,17 +22,66 @@ dep_var_col = 'Total.Points'
 # if so is it just by making it into lots of categorical bins?
 # QUESTION: is it ok to use data available only for treatment
 # when predicting the propensity
-prog_formula = paste0(dep_var_col, ' ~.')
+prog_formula = paste0(dep_var_col, '  ~ Reg_Gender + Reg_GPA + Reg_Acad_Level')
+prop_formula = paste0('factor(', treat_col, ') ~ Reg_Gender + Reg_GPA + Reg_Acad_Level')
+
+# config learning communities
+version = 'lc'
+ifile = "../data_lak_2015/learning_communities2.csv"
+treat_col = 'treat'
+treat_val = 1
+control_val = 0
+dep_var_col = 'CUM_GPA'
+# QUESTION: do binary trees work to predict continuous variables?
+# if so is it just by making it into lots of categorical bins?
+prog_formula = paste0(dep_var_col, ' ~ Sex + ethnic + citizen + parents + income + CUM_GPA')
 prop_formula = paste0('factor(', treat_col, ')~.')
 
-# config model
-hard_reset = FALSE
-hard_reset = TRUE
-tree.number = 2500
+# config problem roulette exam 1
+version = 'pr1'
+ifile = "../data_lak_2015/pr_lak_exam1.csv"
+treat_col = 'tried_xxx'
+treat_val = 'True'
+control_val = 'False'
+dep_var_col = 'Exam1_100'
+# QUESTION: do binary trees work to predict continuous variables?
+# QUESTION: what if we just want to make prediction among users, ignoring control?
+# if so is it just by making it into lots of categorical bins?
+prog_formula = paste0(dep_var_col, ' ~ Reg_Gender + Reg_GPA')
+prop_formula = paste0('factor(', treat_col, ') ~ Reg_GPA + Reg_Gender')
+
+# config problem roulette exam 2
+version = 'pr2'
+ifile = "../data_lak_2015/pr_lak_exam2.csv"
+treat_col = 'tried_xxx'
+treat_val = 'True'
+control_val = 'False'
+dep_var_col = 'Exam2_100'
+# QUESTION: do binary trees work to predict continuous variables?
+# QUESTION: what if we just want to make prediction among users, ignoring control?
+# if so is it just by making it into lots of categorical bins?
+prog_formula = paste0(dep_var_col, ' ~ Reg_Gender + Reg_GPA')
+prop_formula = paste0('factor(', treat_col, ') ~ Reg_GPA + Reg_Gender')
+
+# config problem roulette exam 2
+version = 'pr3'
+ifile = "../data_lak_2015/pr_lak_exam3.csv"
+treat_col = 'tried_xxx'
+treat_val = 'True'
+control_val = 'False'
+dep_var_col = 'Exam3_200'
+# QUESTION: do binary trees work to predict continuous variables?
+# QUESTION: what if we just want to make prediction among users, ignoring control?
+# if so is it just by making it into lots of categorical bins?
+prog_formula = paste0(dep_var_col, ' ~ Reg_Gender + Reg_GPA')
+prop_formula = paste0('factor(', treat_col, ') ~ Reg_GPA + Reg_Gender')
 
 # read and clean data
 idat  = read.csv(ifile)
 temp = na.omit(idat) 
+
+# config model
+tree.number = 2500
 
 #low.data = temp[temp[[treat_col]] == control_val,]
 #high.data = temp[temp[[treat_col]] == treat_val,]
@@ -102,13 +141,8 @@ fm.1.2 = pairmatch(factor(temp[[treat_col]]) ~ ppty.scores , data = temp)
 #cat('--------------------\n\n')
 
 #print(temp[which(fm.1.1!='NA'),])
-print(table(fm.1.1))
+#print(table(fm.1.1))
+print(length(table(fm.1.1)))
 stop('exit')
 
-# pair match based on co-variates only
-fm.1.3 = pairmatch(factor(temp[[treat_col]]) ~ Sex+Init.Ethnic.Group.Descrshort+Init.Citizenship.Status.Descrs+
-                                        stt+Hours.at.entry+Appl.Ctr.Rtng.Cmp1.Val.Descrsh+
-										MaxOfAppl.Ctr.Rtng.Cmp2.Val.Descrsh, data = temp)
-#print(summary(fm.1.3))
-#cat('--------------------\n\n')
 
